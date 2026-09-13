b4cd772991f4a50dd19e94d30eb57375cc274caf
TayyabIftikhar <miftikhr.bscs22seecs@seecs.edu.pk>
2026-09-13 16:53:06 +0500

    Created frontend auth api

---
+++ frontend/src/app.routes.jsx
@@ -0,0 +1,17 @@
+import {createBrowserRouter} from "react-router";
+import Register from "./features/auth/pages/Register.jsx";
+import Login from "./features/auth/pages/Login.jsx";
+
+
+
+export const router = createBrowserRouter([
+    {
+        path: "/register",
+        element: <Register />
+    },
+    {
+        path: "/login",
+        element: <Login />
+    }
+]);
+
---
+++ frontend/src/features/auth/pages/Register.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import {useNavigate, Link} from "react-router";
+
+// const NavigateToLogin = () => {
+//     const navigate = useNavigate();
+//     navigate("/login");
+// }
+
+const handleSubmit = (e) =>{
+    e.preventDefault()
+}
+
+const Register = () => {
+  return (
+        <main>
+        <div className="form-container">
+            <h1>Register</h1>
+            <form onSubmit={handleSubmit}>
+                <div className="input-group">
+                    <label htmlFor="username">Username</label>
+                    <input type="text" id="username" name="username" placeholder="Enter your username" required />
+                </div>
+                <div className="input-group">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" id="email" name="email" placeholder="Enter your email" required />
+                </div>
+                <div className="input-group">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" id="password" name="password" placeholder="Enter your password" required />
+                </div>
+                <button className="button primary-button" type="submit">Register</button>
+
+            </form>
+
+            <p>Already have an account? <Link to="/login">Login</Link></p>
+
+
+        </div>
+    </main>
+  )
+}
+
+export default Register
